2a1ba9e90bfd4f4e7784a827fc117fbb25b4a980
jiebao <908822571@qq.com>
2021-07-29 18:02:08 +0800

    实现form表单验证  并且跳转到相应页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <el-row :gutter="20">
-      <el-col :span="8" :offset="8">
+    <el-row type="flex" justify="center">
+      <el-col :span="8" >
         <div class="grid-content"></div>
       </el-col>
     </el-row>
 
-    <el-row  :gutter="20">
-      <el-col :span="8" :offset="8">
+    <el-row  type="flex" justify="center">
+      <el-col :span="8" >
         <el-card shadow="always">
-          <h1>活动表格</h1>
+          <h1 style="text-align: center">登录页面</h1>
           <el-divider></el-divider>
         <el-form :model="ValidateForm" ref="ValidateForm" label-width="100px" class="demo-ruleForm">
           <el-form-item
@@ -30,6 +30,20 @@
           >
             <el-input  placeholder="请输入密码" v-model="ValidateForm.password" show-password></el-input>
           </el-form-item>
+          <el-radio-group v-model="radio">
+            <el-radio :label=false>仅前端展示</el-radio>
+            <el-radio :label=true>发送给后端</el-radio>
+          </el-radio-group>
+          <el-form-item
+            label="后端IP地址"
+            prop="ipp"
+            :rules="[
+                { required: true, message: '后端IP不能为空'},
+              ]"
+            v-show="radio"
+          >
+            <el-input   v-model="ValidateForm.ipp" ></el-input>
+          </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="submitForm('ValidateForm')">提交</el-button>
             <el-button @click="resetForm('ValidateForm')">重置</el-button>
@@ -48,16 +62,18 @@ export default {
     return {
        ValidateForm: {
         name: '',
-        password: ''
+        password: '',
+         ipp:'http://192.168.101.55/',
       },
-
+      radio: false
     };
   },
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert('Name:'+ this.ValidateForm.name +';'+ 'password:' + this.ValidateForm.password);
+          alert('Name:'+ this.ValidateForm.name +';'+ 'password:' + this.ValidateForm.password + ";"+'IP:'+ this.ValidateForm.ipp);
+          this.$router.replace({ path: '/continue'})
         } else {
           console.log('error submit!!');
           return false;
@@ -72,6 +88,11 @@ export default {
 </script>
 
 <style scoped>
+.el-radio-group {
+  display: flex;
+  justify-content: center;
+  padding-bottom: 20px;
+}
 .grid-content {
   /* background: rgb(14, 214, 131); */
   border-radius: 4px;
