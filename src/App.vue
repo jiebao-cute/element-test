52ffdb38b206e3ea3cbc87fbadc38abee3e1c804
jiebao <908822571@qq.com>
2021-08-10 13:17:53 +0800

    引入icon

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,58 @@
 <template>
   <div id="app">
+       <sidebar/>
         <router-view></router-view>
   </div>
 </template>
 
 <script>
+import sidebar from '@/components/common/Sidebar.vue'
 export default {
   name: 'App',
+  components:{
+    sidebar
+  }
 
 }
 </script>
 
 <style>
-#app {
-
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
-body{
-  line-height: 1.5;
+html , body, #app{
+  height: 100%;
+}
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #eee;
 }
+a {
+  text-decoration:  none;
+  color: #444;
+}
+ul,li {
+  list-style: none;
+}
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0 0 2px 0 #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+#app {
+  display: flex;
+  align-items: stretch;
+}
+
 
 </style>
---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="sidebar">
+    <div class="icons">
+      <router-link to="/note/1" title="笔记">
+        <svg class='iconfont icon-note'>
+          <use xlink:href="#icon-biji"/>
+        </svg>
+      </router-link>
+      <router-link to="/notebooks" title="笔记本">
+        <svg class="iconfont icon-notebook">
+          <use xlink:href="#icon-dianhua"/>
+        </svg>
+      </router-link>
+      <router-link to="/trash/2" title="回收站">
+        <svg class="iconfont icon-trash">
+          <use xlink:href="#icon-huishou"/>
+        </svg>
+      </router-link>
+    </div>
+    <div class="logout">
+      <svg class="iconfont icon-logout">
+        <use xlink:href="#icon-tuichu"/>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.icons{
+  margin-top: 20px;
+  display: flex;
+  flex-direction:column
+}
+.icons a {
+  padding: 6px 0;
+  display: block;
+}
+.icons .router-link-active {
+  background-color: #5e6266;
+}
+.iconfont {
+  width: 2em;
+  height: 2em;
+  fill: currentColor;
+  overflow: hidden;
+  color: #fff
+}
+
+.sidebar {
+  position: relative;
+  width: 56px;
+  text-align: center;
+  background-color: #2c333c;
+}
+.logout {
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+
+}
+</style>
