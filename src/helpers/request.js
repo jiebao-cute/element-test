a33542314153dd9bf35400ec58dfc51aab118630
jiebao <908822571@qq.com>
2021-08-12 12:57:03 +0800

    解决在开发时登录报 “未登录”bug

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.baseURL = 'http://note-server.hunger-valley.com'
+axios.defaults.baseURL = 'https://note-server.hunger-valley.com'
 axios.defaults.withCredentials = true
 
 export default function request(url,type='GET',data={}){
