3d6dad6fb1ae8794d926397b0c319d8c697fb75d
jiebao <908822571@qq.com>
2021-08-12 19:27:14 +0800

    生产环境和开发环境baseURL 无缝切换

---
+++ build/mock.config.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+const mockBaseURL = 'http://localhost:3000' //本地设置地址
+const realBaseURL = 'http://note-server.hunger-valley.com' //线上地址
+
+exports.config = function ({isDev = true} = {isDev: true}) {//判断是否为开发环境，默认是true
+  let filetxt = `
+  module.exports = {
+     baseURL: '${isDev ? mockBaseURL : realBaseURL}'
+}
+`
+  fs.writeFileSync(path.join(__dirname, '../src/helpers/config-baseURL.js'),filetxt)
+}
--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
+import baseURLConfig from './config-baseURL'
+
+
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.baseURL = 'https://note-server.hunger-valley.com'
+axios.defaults.baseURL = baseURLConfig.baseURL
 axios.defaults.withCredentials = true
 
 export default function request(url,type='GET',data={}){
