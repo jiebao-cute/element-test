2a5cfdf6c91cd76fc5968b7cbd6c75e62ffa882a
jiebao <908822571@qq.com>
2021-08-17 21:05:01 +0800

    笔记本列表静态页面

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,34 +1,142 @@
 <template>
   <div id="notebook-list">
-    <h1>{{msg}}</h1>
-    <ul>
-      <li><router-link to="/note/1">笔记本1</router-link></li>
-      <li><router-link to="/note/2">笔记本2</router-link></li>
-    </ul>
+    <header>
+      <a href="#" class="btn">
+        <svg class='iconfont icon-plus'>
+          <use xlink:href="#icon-jia"/>
+        </svg>
+        新建笔记
+      </a>
+    </header>
+    <main>
+      <div class="layout">
+        <h3>笔记列表</h3>
+        <div class="book-list">
+          <a href="#" class="notebook">
+            <div>
+              <span>
+              <svg class='iconfont icon-notebook'>
+                <use xlink:href="#icon-biji"/>
+              </svg>
+                </span>
+              笔记标题1
+              <span>3</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="data">3天前</span>
+            </div>
+          </a>
+          <a href="#" class="notebook">
+            <div>
+              <span>
+              <svg class='iconfont icon-notebook'>
+                <use xlink:href="#icon-biji"/>
+              </svg>
+              </span>
+              笔记标题2
+              <span>3</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="data">5天前</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 import Auth from "../apis/auth.js";
+
 export default {
   name: 'NoteboonList',
-  data () {
+  data() {
     return {
       msg: '笔记本列表'
     }
   },
   created() {
-    Auth.getInfo().then(res=>{
-      if(!res.isLogin){
-        this.$router.push({ path : '/'})
+    Auth.getInfo().then(res => {
+      if (!res.isLogin) {
+        this.$router.push({path: '/'})
       }
     })
   }
 }
 </script>
 
-<style scoped>
-h1 {
-  color: red;
+<style lang="less" scoped>
+.iconfont {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+#notebook-list{
+  flex: 1;
+  .btn{
+    font-size: 12px;
+    color: #666;
+    cursor: pointer;
+    margin-left: 10px;
+  }
+  .btn .iconfont{
+    font-size: 12px;
+  }
+  header {
+    padding: 12px;
+    border-bottom: 1px solid #ccc;
+  }
+  main {
+    padding: 30px 40px;
+  }
+  .layout {
+    max-width: 966px;
+    margin: 0 auto;
+  }
+   main h3{
+     font-size: 14px;
+     color: #000;
+   }
+  main .book-list{
+    margin-top: 10px;
+    font-size: 14px;
+    color: #666;
+    background-color: #ffffff;
+    border-radius: 4px;
+    font-weight: bold;
+  }
+  main .book-list span{
+    font-size: 12px;
+    font-weight: bold;
+    color: #b3c0c8;
+  }
+
+  main .data,
+  main .action{
+    float: right;
+    margin-left:  10px;
+  }
+  main .iconfont{
+    color: #b3c0c8;
+    margin-right: 4px;
+  }
+  main .notebook {
+    display: block;
+    cursor: pointer;
+  }
+  main a.notebook:hover{
+    background-color: #f7fafd;
+  }
+  main a.notebook div{
+    border-bottom: 1px solid #ebebeb;
+    padding: 12px 14px;
+  }
+  main .error-msg{
+    font-size: 12px;
+    color: red;
+  }
 }
 </style>
