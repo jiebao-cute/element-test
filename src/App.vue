a53932dccefc35ef541d3021c00ffc1dbc193706
jiebao <908822571@qq.com>
2021-08-18 21:22:49 +0800

    使用element组件库  编写弹框 修改样式

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 *{
   box-sizing: border-box;
   margin: 0;
@@ -31,6 +31,39 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: #eee;
+}
+.el-message-box__wrapper .el-message-box .el-message-box__header{
+  margin: 0 0;
+  background:  #666666;
+  .el-message-box__title{
+    color: white;
+  }
+}
+.el-button--primary {
+  background:  #444444;
+  border: 1px solid #444444;
+}
+.el-button:hover {
+  background: #cccccc;
+  border: 1px solid #cccccc;
+  color: black;
+}
+.el-button--primary:focus{
+  background:  #444444;
+  border: 1px solid #444444;
+}
+
+.el-input__inner:focus{
+  border: 1px solid #444;
+}
+input{
+  width: 300px;
+  height: 30px;
+  line-height: 30px;
+  border: 1px solid #cccccc;
+  border-radius: 3px;
+  padding: 3px 5px;
+  outline: none;
 }
 a {
   text-decoration:  none;
--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -10,7 +10,7 @@
     </header>
     <main>
       <div class="layout">
-        <h3>笔记列表({{notebooks.length}})</h3>
+        <h3>笔记列表({{ notebooks.length }})</h3>
         <div class="book-list">
           <router-link to="id" class="notebook" v-for="notebooks  in notebooks" :key="notebooks.id">
             <div>
@@ -19,11 +19,11 @@
                 <use xlink:href="#icon-biji"/>
               </svg>
                 </span>
-              {{notebooks.title}}
-              <span>{{notebooks.noteCounts}}</span>
+              {{ notebooks.title }}
+              <span>{{ notebooks.noteCounts }}</span>
               <span class="action" @click.stop.prevent="onEdit(notebooks)">编辑</span>
               <span class="action" @click.stop.prevent="onDelete(notebooks)">删除</span>
-              <span class="data">{{notebooks.friendlycreatedAt}}</span>
+              <span class="data">{{ notebooks.friendlycreatedAt }}</span>
             </div>
           </router-link>
         </div>
@@ -46,38 +46,66 @@ export default {
       notebooks: []
     }
   },
-  methods:{
-    onCreat(){
-      let title = window.prompt('创建笔记本')
-      if (title.trim() === '' ){
-        alert('笔记名不能为空')
-        return
-      }
-      Notebooks.addNoteBook({title}).then(res=>{
-        console.log(res)
+  methods: {
+    onCreat() {
+      this.$prompt('请输入笔记本标题', '创建笔记本', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: '标题不能为空，且不能超过三十个字'
+      }).then(({value}) => {
+        return Notebooks.addNoteBook({title: value})
+      }).then(res => {
         res.data.friendlycreatedAt = friendlyDate(res.data.createdAt)//UI上面显示的时间
         this.notebooks.unshift(res.data)
-        alert(res.msg)
-      })
-    },
-    onEdit(notebooks){
-      let title = window.prompt('修改标题',notebooks.title)
-      Notebooks.updateNotebook(notebooks.id,{title}).then(res=>{
-        console.log(res)
-        notebooks.title = title
-        alert(res.msg)
-      })
-    },
-    onDelete(notebooks){
-      let confirm = window.confirm('你确定要删除吗？')
-      if(confirm){
-        Notebooks.deleteNotebook(notebooks.id).then(res=>{//通知服务器删除
-          console.log(res)
-          this.notebooks.splice(this.notebooks.indexOf(notebooks),1)//通知UI删除
-          alert(res.msg)
+        this.$message({
+          type: 'success',
+          message: res.msg
         })
+      }).catch((res) => {
+        this.$message({
+          type: 'error',
+          message: '取消创建'
+        });
+      });
+    },
+    onEdit(notebooks) {
+      let title = ''
+      this.$prompt('请输入新笔记本标题', '更新笔记本标题', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: '标题不能为空，且不能超过三十个字'
+      }).then(({value}) => {
+        title = value
+        return Notebooks.updateNotebook(notebooks.id, {title})
+      }).then(res => {
+        notebooks.title = title
+        this.$message({
+          type: 'success',
+          message: res.msg
+        })
+      }).catch((res) => {
+        this.$message({
+          type: 'error',
+          message: '取消更新'
+        });
+      });
+    },
+    onDelete(notebooks) {
+      this.$confirm('此操作将永久删除该文件, 你确定要删除该笔记吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        return Notebooks.deleteNotebook(notebooks.id)
+      }).then(res => {//通知服务器删除
+        this.notebooks.splice(this.notebooks.indexOf(notebooks), 1)//通知UI删除
+        this.$message({
+          type: 'success',
+          message: res.msg,
+        })})
       }
-    }
   },
   created() {
     Auth.getInfo().then(res => {
@@ -85,7 +113,7 @@ export default {
         this.$router.push({path: '/'})
       }
     })
-    Notebooks.getAll().then(res=>{
+    Notebooks.getAll().then(res => {
       this.notebooks = res.data
     })
   }
@@ -100,9 +128,11 @@ export default {
   fill: currentColor;
   overflow: hidden;
 }
-#notebook-list{
+
+#notebook-list {
   flex: 1;
-  .btn{
+
+  .btn {
     font-size: 12px;
     color: #666;
     cursor: pointer;
@@ -110,25 +140,31 @@ export default {
     align-items: center;
     padding: 5px 10px;
   }
-  .btn .iconfont{
+
+  .btn .iconfont {
     font-size: 12px;
   }
+
   header {
     padding: 14px;
     border-bottom: 1px solid #ccc;
   }
+
   main {
     padding: 30px 40px;
   }
+
   .layout {
     max-width: 966px;
     margin: 0 auto;
   }
-   main h3{
-     font-size: 14px;
-     color: #000;
-   }
-  main .book-list{
+
+  main h3 {
+    font-size: 14px;
+    color: #000;
+  }
+
+  main .book-list {
     margin-top: 10px;
     font-size: 14px;
     color: #666;
@@ -136,33 +172,39 @@ export default {
     border-radius: 4px;
     font-weight: bold;
   }
-  main .book-list span{
+
+  main .book-list span {
     font-size: 12px;
     font-weight: bold;
     color: #b3c0c8;
   }
 
   main .data,
-  main .action{
+  main .action {
     float: right;
-    margin-left:  10px;
+    margin-left: 10px;
   }
-  main .iconfont{
+
+  main .iconfont {
     color: #b3c0c8;
     margin-right: 4px;
   }
+
   main .notebook {
     display: block;
     cursor: pointer;
   }
-  main a.notebook:hover{
+
+  main a.notebook:hover {
     background-color: #f7fafd;
   }
-  main a.notebook div{
+
+  main a.notebook div {
     border-bottom: 1px solid #ebebeb;
     padding: 12px 14px;
   }
-  main .error-msg{
+
+  main .error-msg {
     font-size: 12px;
     color: red;
   }
