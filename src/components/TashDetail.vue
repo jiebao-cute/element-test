21dae4d88a5cfa369106b254e1dc36b8d8d6307b
jiebao <908822571@qq.com>
2021-09-08 13:18:00 +0800

    回收站CSS样式完成

--- src/components/TashDetail.vue
+++ src/components/TashDetail.vue
@@ -1,33 +1,91 @@
 <template>
-  <div id="trash-detail">
-    <h1>{{msg}} : {{ $route.params.noteId }}</h1>
+  <div id="trash" class="detail">
+    <div class="note-sidebar">
+      <h3 class="notebook-title">回收站</h3>
+      <div class="menu">
+        <div>更新时间</div>
+        <div>标题</div>
+      </div>
+      <ul class="notes">
+        <li v-for="note in curTrashNote">
+          <router-link :to="`/trash?noteId=${note.id}`">
+            <span class="date">{{note.updatedAtFriendly}}</span>
+            <span class="title">{{note.title}}</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="note-detail">
+      <div class="note-bar" v-if="true">
+        <span> 创建日期: {{curTrashNote.createdAtFriendly}}</span>
+        <span> | </span>
+        <span> 更新日期: {{curTrashNote.updatedAtFriendly}}</span>
+        <span> | </span>
+        <span> 所属笔记本: {{belongTo}}</span>
+
+        <a class="btn action" @click="onRevert">恢复</a>
+        <a class="btn action" @click="onDelete">彻底删除</a>
+      </div>
+      <div class="note-title">
+        <span>{{curTrashNote.title}}</span>
+      </div>
+      <div class="editor">
+        <div class="preview markdown-body" v-html=""></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Auth from "../apis/auth";
-import {mapActions} from "vuex";
+import MarkdownIt from 'markdown-it'
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+let md = new MarkdownIt()
 
 export default {
   name: 'TashDetail',
-  data () {
+  data() {
     return {
-      msg: '回收站笔记详情页'
+      curTrashNote:{
+        id: 3,
+        title: '我的笔记',
+        content: 'hello',
+        updatedAtFriendly: '两个小时前',
+        createdAtFriendly: '三个小时前'
+      },
+      belongTo:'我的笔记本'
     }
   },
-  created() {
-    this.checkLogin({ path: '/login' })
-  },
   methods:{
-    ...mapActions([
-      'checkLogin'
-    ])
+    onRevert(){
+      console.log('1')
+    },
+    onDelete(){
+      console.log('2')
+    }
   }
 }
 </script>
 
-<style scoped>
-h1 {
-  color: blue;
+<style lang="less" scoped>
+
+@import url(../assets/css/note-sidebar.less);
+@import url(../assets/css/note-detail.less);
+
+#trash {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
+
+  .note-bar {
+    .action {
+      float: right;
+      margin-left: 10px;
+      padding: 2px 4px;
+      font-size: 12px;
+
+    }
+  }
 }
+
 </style>
