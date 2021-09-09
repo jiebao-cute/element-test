42b9780d3f8c7e69d331e5a71108fb193e0b0df3
jiebao <908822571@qq.com>
2021-09-09 09:32:30 +0800

    build配置出问题  修改配置

--- build/mock.config.js
+++ build/mock.config.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-const mockBaseURL = 'https://note-server.hunger-valley.com' //本地设置地址
-const realBaseURL = 'http://note-server.hunger-valley.com' //线上地址
+const mockBaseURL = '//note-server.hunger-valley.com' //本地设置地址
+const realBaseURL = '//note-server.hunger-valley.com' //线上地址
 
 exports.config = function ({isDev = true} = {isDev: true}) {//判断是否为开发环境，默认是true
   let filetxt = `
